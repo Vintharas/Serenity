17d544c15e9813c6bd98c9019378155e19cd2f28
vintharas <jaime.gg83@gmail.com>
2013-01-27 07:59:37 +0100

    Title Screen: Added a simple fading blinking text that prompts the player to start playing
       Created a simple BlinkingText class to simulate a blinking text

---
+++ js-using-requirejs/BlinkingText.js
@@ -0,0 +1,40 @@
+// BlinkingText.js
+define([],
+    function(){
+        function BlinkingText(text, fillStyle, font, x, y, blinkingPeriod){
+            this.text = text;
+            this.fillStyle = fillStyle;
+            this.font = font;
+            this.x = x;
+            this.y = y;
+            this.blinkingPeriod = blinkingPeriod;
+            this.lastBlink = (new Date()).getTime();
+            this.currentAlpha = 1;
+            this.isFadingOut = true
+            this.update = function(){
+                var currentTime = (new Date()).getTime();
+                var timePassedSinceLastBlink = currentTime - this.lastBlink;
+                if (timePassedSinceLastBlink >= this.blinkingPeriod){
+                    this.lastBlink = currentTime;
+                    if (this.isFadingOut) this.currentAlpha = 0;
+                    else this.currentAlpha = 1;
+                    this.isFadingOut = !this.isFadingOut;
+                }
+                else {
+                    if (this.isFadingOut) this.currentAlpha = 1 - timePassedSinceLastBlink/this.blinkingPeriod;
+                    else this.currentAlpha = timePassedSinceLastBlink/this.blinkingPeriod;
+                }
+            }
+            this.draw = function(){
+                var canvas = document.getElementById("gamecanvas");
+                var context = canvas.getContext("2d");
+                context.globalAlpha = this.currentAlpha;
+                context.fillStyle = this.fillStyle;
+                context.font = this.font;
+                context.fillText(this.text, this.x, this.y)
+                context.globalAlpha = 1;
+            };
+        }
+        return BlinkingText;
+    }
+);
--- js-using-requirejs/main.js
+++ js-using-requirejs/main.js
@@ -1,6 +1,6 @@
 // main.js
-require(["Game", "Scene", "SolidBackground", "Text"],
-   function(Game, Scene, SolidBackground, Text) {
+require(["Game", "Scene", "SolidBackground", "Text", "BlinkingText"],
+   function(Game, Scene, SolidBackground, Text, BlinkingText) {
         // initialize game
         var game = new Game();
         // initialize scenes of the game
@@ -11,6 +11,8 @@ require(["Game", "Scene", "SolidBackground", "Text"],
         titleScene.gameObjects.push(title);
         var subtitle = new Text("a fanfic space shooter based on the Firefly universe", "#AAA", "12px sans-serif", 100, 225);
         titleScene.gameObjects.push(subtitle);
+        var startText = new BlinkingText("Start!", "#b9090b", "24px sans-serif", 200, 400, 2000);
+        titleScene.gameObjects.push(startText);
         game.scenes.push(titleScene);
         // start game
         game.intervalId = setInterval(game.run, 1000/60); // 60fps max
